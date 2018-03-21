6c7dc35792e44def0b9b5a8d865cd85d0999afdd
deben <deben@wacai.com>
2018-03-21 10:47:25 +0800

    nn

--- nn/nn.py
+++ nn/nn.py
@@ -17,8 +17,8 @@ label = input_data[..., 2]
 label_info = np.vstack([label, 1 - label]).T
 sample_size = len(input_data)
 
-reglamda = 0.4
-epsion = 0.01
+reglamda = 12
+epsion = 0.0001
 
 
 def train(input_x):
@@ -38,7 +38,6 @@ def train(input_x):
         h2 = np.dot(o1, W2) + b2  # h2 4*5 dot 5*6 = 4*6
         o2 = tanh_f(h2)
         # print("o2 shape ->", o2.shape, o2)
-
         h3 = np.dot(o2, W3) + b3  # h3 4*6 dot 6*2 = 4*2
         o3 = softmax_f(h3)
         # print("o3 shape ->", o3.shape, o3)
@@ -48,7 +47,6 @@ def train(input_x):
         loss += reglamda / 2 * (np.sum(np.square(W1)) + np.sum(np.square(W2) + np.sum(np.square(W3))))
 
         # backward
-        # 4*2
         delta3 = o3 - label_info  # 4*2
         dw3 = (o2.T).dot(delta3)  # 6*2
         db3 = np.sum(delta3, axis=0, keepdims=True)
@@ -57,14 +55,15 @@ def train(input_x):
         db2 = np.sum(delta2, axis=0, keepdims=True)
         # 4*6 5*6 4*5 =>4*5
         delta1 = delta2.dot(W2.T) * (sigmoid_f(h1) - np.power(sigmoid_f(h1), 2))
-
         dw1 = (input_x.T).dot(delta1)  # 4*2 4*5 => 2*5
         db1 = np.sum(delta1, axis=0, keepdims=True)  # 4*1
 
+        # 对正则化项的偏导
         dw1 += reglamda * W1
         dw2 += reglamda * W2
         dw3 += reglamda * W3
 
+        # 梯度更新部分
         W1 += -epsion * dw1
         b1 += -epsion * db1
         W2 += -epsion * dw2
@@ -74,14 +73,7 @@ def train(input_x):
 
         if (i % 50 == 0):
             print("loss -> ", loss)
-    return  {
-        "W1": W1,
-        "b1": b1,
-        "W2": W2,
-        "b2": b2,
-        "W3": W3,
-        "b3": b3,
-    }
+    return  { "W1": W1, "b1": b1, "W2": W2, "b2": b2, "W3": W3, "b3": b3 }
 
 model_weight = train(input_x)
 print(model_weight)
